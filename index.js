b668d5be704f2b2ad4be9a72551890e2a3446d4f
Josh Randall <josh-randall@hush.ai>
2021-10-05 23:49:30 -0400

    Fix exports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import RasterBackgroundmapMetadata from '/src/types/raster-backgroundmap-metadata.ts'
-import CellXY                      from '/src/types/cell-x-y.ts'
+import _RasterBackgroundmapMetadata from '/src/types/raster-backgroundmap-metadata.ts'
+import _CellXY                      from '/src/types/cell-x-y.ts'
 
-import BACKGROUNDMAP_BOUND_LENGTH from '/src/types/backgroundmap-bound-length.ts'
+import _BACKGROUNDMAP_BOUND_LENGTH from '/src/types/backgroundmap-bound-length.ts'
 
-export const CellXY = CellXY;
-export const RasterBackgroundmapMetadata = RasterBackgroundmapMetadata;
-export const BACKGROUNDMAP_BOUND_LENGTH = BACKGROUNDMAP_BOUND_LENGTH;
+export const CellXY = _CellXY;
+export const RasterBackgroundmapMetadata = _RasterBackgroundmapMetadata;
+export const BACKGROUNDMAP_BOUND_LENGTH = _BACKGROUNDMAP_BOUND_LENGTH;
