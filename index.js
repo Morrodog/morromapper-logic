1f28a6336253f536ec15c40ca60e89524e9c7dc5
Josh Randall <josh-randall@hush.ai>
2021-10-06 00:06:43 -0400

    Fix incorrect path for bound length

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { default as RasterBackgroundmapMetadata } from './src/types/raster-backgroundmap-metadata.ts'
-export { default as BACKGROUNDMAP_BOUND_LENGTH }  from './src/types/backgroundmap-bound-length.ts'
+export { default as BACKGROUNDMAP_BOUND_LENGTH }  from './src/constants/backgroundmap-bound-length.ts'
 export { default as CellXY }                      from './src/types/cell-x-y.ts'
