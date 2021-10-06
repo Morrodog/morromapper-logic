d40fc268dc1b796b779313ad91140b59fb608a83
Josh Randall <josh-randall@hush.ai>
2021-10-06 00:05:21 -0400

    Experimental module resolution changes

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import _RasterBackgroundmapMetadata from '/src/types/raster-backgroundmap-metadata.ts'
-import _CellXY                      from '/src/types/cell-x-y.ts'
-
-import _BACKGROUNDMAP_BOUND_LENGTH from '/src/types/backgroundmap-bound-length.ts'
-
-export const CellXY = _CellXY;
-export const RasterBackgroundmapMetadata = _RasterBackgroundmapMetadata;
-export const BACKGROUNDMAP_BOUND_LENGTH = _BACKGROUNDMAP_BOUND_LENGTH;
+export { default as RasterBackgroundmapMetadata } from './src/types/raster-backgroundmap-metadata.ts'
+export { default as BACKGROUNDMAP_BOUND_LENGTH }  from './src/types/backgroundmap-bound-length.ts'
+export { default as CellXY }                      from './src/types/cell-x-y.ts'
