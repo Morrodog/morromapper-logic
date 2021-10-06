d0132f9c6bfc324ca845d01b4d9af405921bff40
Josh Randall <josh-randall@hush.ai>
2021-10-05 22:59:08 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Morromapper Logic
+This repository contains core logic that is common to Morromapper Components and to the maps that use Morromapper Components, or for logic that generates data for maps that use Morromapper Components.
---
+++ index.js
@@ -0,0 +1,8 @@
+import RasterBackgroundmapMetadata from '/src/types/raster-backgroundmap-metadata.ts'
+import CellXY                      from '/src/types/cell-x-y.ts'
+
+import backgroundmapBoundLength from '/src/types/backgroundmap-bound-length.ts'
+
+export CellXY CellXY;
+export RasterBackgroundmapMetadata RasterBackgroundmapMetadata;
+export backgroundmapBoundLength backgroundmapBoundLength;
